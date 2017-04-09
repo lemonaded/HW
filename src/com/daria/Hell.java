8d86e38ce767c777a3c558c617271ac490b6f91c
lemonaded <workwithus@bk.ru>
2017-04-09 22:55:52 +0300

    changed two to one

--- src/com/daria/Hell.java
+++ src/com/daria/Hell.java
@@ -3,6 +3,6 @@ package com.daria;
 public class Hell {
 
     public static void main(String[] args) {
-        System.out.println("hello dggg " + args[2]);
+        System.out.println("hello dggg " + args[1]);
     }
 }
