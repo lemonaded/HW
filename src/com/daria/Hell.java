9992c1f31efe93c806f8e2eac75ec0107d771fbc
lemonaded <workwithus@bk.ru>
2017-04-10 00:15:21 +0300

    if

--- src/com/daria/Hell.java
+++ src/com/daria/Hell.java
@@ -2,10 +2,14 @@ package com.daria;
 
 public class Hell {
 
-    public static void main(String[] args) {
-        for (int i = 20; i <= 45 ; i++) {
-            System.out.println(i);
-        }
+    public static void main(String[] args)
+    {
         System.out.println("finish");
+        for (int i = 20; i <= 45; i++) {
+            if (i == 40) {
+                System.out.println(i);
+            }
+
+        }
     }
 }
