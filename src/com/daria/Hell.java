39b9a229f985c2de14b2fb161f9ecd5e84226c25
lemonaded <workwithus@bk.ru>
2017-04-23 18:50:42 +0300

    for

--- src/com/daria/Hell.java
+++ src/com/daria/Hell.java
@@ -3,10 +3,12 @@ package com.daria;
 public class Hell {
 
     public static void main(String[] args) {
-int i = 0;
-while(i < 5) {
-    System.out.println("i = " +i);
-    i++;
+        // это вроде уже было :)
+        for (int i = 0; i < 5; i++){
+            System.out.println("i = " +i);
+        }
+
+
 }
     }
-}
+
