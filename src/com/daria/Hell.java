79edd1216c7601264e9cfee310f52f456725c429
lemonaded <workwithus@bk.ru>
2017-04-23 18:44:57 +0300

    while

--- src/com/daria/Hell.java
+++ src/com/daria/Hell.java
@@ -2,20 +2,11 @@ package com.daria;
 
 public class Hell {
 
-    public static void main(String[] args)
-    {
-        int a = 4;
-        int b = 9;
-
-        if(a < b) {
-            if(a < b){
-                System.out.println("ololo_true");
-            } else{
-                System.out.println("ololo_false");
-            }
-        } else {
-            System.out.println("false");
-        }
-        }
+    public static void main(String[] args) {
+int i = 0;
+while(i < 5) {
+    System.out.println("i = " +i);
+    i++;
+}
     }
-
+}
