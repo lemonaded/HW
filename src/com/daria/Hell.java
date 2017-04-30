8651b994467e55ab1b7067e0ced1f63ef94fdce6
lemonaded <workwithus@bk.ru>
2017-04-30 18:58:07 +0300

    create x2-x5

--- src/com/daria/Hell.java
+++ src/com/daria/Hell.java
@@ -5,8 +5,17 @@ public class Hell {
     public static void main(String[] args) {
 
         Heaven x = new Heaven();
-        x.setPopulation(5);
-        System.out.println(x.getPopulation());
+        Heaven x2 = new Heaven();
+        Heaven x3 = new Heaven();
+        Heaven x4 = new Heaven();
+        Heaven x5 =  new Heaven();
+        x.setPopulation(101);
+        x2.setPopulation(112);
+        x3.setPopulation(156);
+        x4.setPopulation(191);
+        x5.setPopulation(104);
+        System.out.println("Avenir,im confused! " );
+
 
     }
 }
